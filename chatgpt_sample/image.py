ed9ad0ec04259a94fd57d30190ed4d497ed4c7fd
shota <sspydery0@gmail.com>
2023-04-12 17:54:34 +0900

    mod

--- chatgpt_sample/image.py
+++ chatgpt_sample/image.py
@@ -1,33 +1,38 @@
 import openai
 import settings
 import requests
-import json
+import datetime
 
 prompt = """
 頭が赤い魚を食べた猫
 """
 
 def create_image_from_text(text):
-     # api key の設定
+    # api key の設定
     openai.api_key = settings.api_key
 
     # 応答設定
     response = openai.Image.create(
-                  prompt = text,             # 画像生成に用いる説明文章
-                  n = 1,                     # 何枚の画像を生成するか
-                  size = '512x512',          # 画像サイズ
-                  response_format = "url"    # API応答のフォーマット
-                )
+        # プロンプト
+        prompt = text,
+        # 何枚の画像を生成するか  1〜10
+        n = 1,
+        # 画像サイズ 256x256、512x512、1024x1024
+        size = '512x512',
+        # API応答のフォーマット url b64_json
+        response_format = 'url'
+        )
 
     # API応答から画像URLを指定
     image_url = response['data'][0]['url']
-    
+
     # 画像をローカルに保存
+    dt_now = datetime.datetime.now()
     image_data = requests.get(image_url).content
-    with open("chat-gpt-generated-image.jpg", "wb") as f:
+    with open(f"chatgpt_image_{dt_now.strftime('%Y-%m-%d-%H-%M-%S')}.jpg", "wb") as f:
         f.write(image_data)
-        
+
     return image_url
 
 if __name__ == '__main__':
-    create_image_from_text(prompt)
+    print(create_image_from_text(prompt))
